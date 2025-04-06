5542b2abd146e84146ba86d8b6082257c71a3576
shimada <curryside.net@gmail.com>
2025-04-06 10:56:04 +0000

    test

--- main_test.go
+++ main_test.go
@@ -2,10 +2,10 @@ package main_test
 
 import (
 	"net/url"
-	"reflect"
 	"testing"
 
 	main "github.com/dev-shimada/api-stubs"
+	"github.com/google/go-cmp/cmp"
 )
 
 func Test_pathMatcher(t *testing.T) {
@@ -249,7 +249,130 @@ func Test_queryMatcher(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "all",
+			args: args{
+				endpoint: main.Endpoint{
+					Request: main.Request{
+						QueryParameters: map[string]main.Matcher{
+							"param1": {
+								EqualTo: "12345",
+							},
+							"param2": {
+								Matches: "[0-9]{5}",
+							},
+							"param3": {
+								DoesNotMatch: "[a-z]{5}",
+							},
+							"param4": {
+								Contains: "abc",
+							},
+							"param5": {
+								DoesNotContain: "xyz",
+							},
+						},
+					},
+				},
+				gotQuery: url.Values{
+					"param1": []string{"12345"},
+					"param2": []string{"67890"},
+					"param3": []string{"00000"},
+					"param4": []string{"abcxyz"},
+					"param5": []string{"12345"},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "equalTo false",
+			args: args{
+				endpoint: main.Endpoint{
+					Request: main.Request{
+						QueryParameters: map[string]main.Matcher{
+							"param": {
+								EqualTo: "12345",
+							},
+						},
+					},
+				},
+				gotQuery: url.Values{
+					"param": []string{"123456"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "contains false",
+			args: args{
+				endpoint: main.Endpoint{
+					Request: main.Request{
+						QueryParameters: map[string]main.Matcher{
+							"param": {
+								Contains: "12345",
+							},
+						},
+					},
+				},
+				gotQuery: url.Values{
+					"param": []string{"1234"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "doesNotContain false",
+			args: args{
+				endpoint: main.Endpoint{
+					Request: main.Request{
+						QueryParameters: map[string]main.Matcher{
+							"param": {
+								DoesNotContain: "12345",
+							},
+						},
+					},
+				},
+				gotQuery: url.Values{
+					"param": []string{"12345abc"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "matches false",
+			args: args{
+				endpoint: main.Endpoint{
+					Request: main.Request{
+						QueryParameters: map[string]main.Matcher{
+							"param": {
+								Matches: "[0-9]{5}",
+							},
+						},
+					},
+				},
+				gotQuery: url.Values{
+					"param": []string{"1234"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "doesNotMatch false",
+			args: args{
+				endpoint: main.Endpoint{
+					Request: main.Request{
+						QueryParameters: map[string]main.Matcher{
+							"param": {
+								DoesNotMatch: "[0-9]{5}",
+							},
+						},
+					},
+				},
+				gotQuery: url.Values{
+					"param": []string{"12345abc"},
+				},
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -270,7 +393,59 @@ func Test_loadConfig(t *testing.T) {
 		want    []main.Endpoint
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "valid config",
+			args: args{
+				filePath: "testdata/test_config.json",
+			},
+			want: []main.Endpoint{
+				{
+					Request: main.Request{
+						URLPathTemplate: "/example/{path1}/{path2}/{path3}/{path4}/{path5}",
+						Method:          "GET",
+						PathParameters: map[string]main.Matcher{
+							"path1": {
+								EqualTo: "v1",
+							},
+							"path2": {
+								Matches: "^[a-zA-Z0-9]{3}$",
+							},
+							"path3": {
+								DoesNotMatch: "[a-zA-Z]{3}",
+							},
+							"path4": {
+								Contains: "a",
+							},
+							"path5": {
+								DoesNotContain: "b",
+							},
+						},
+						QueryParameters: map[string]main.Matcher{
+							"param1": {
+								EqualTo: "value1",
+							},
+							"param2": {
+								Matches: "[a-zA-Z0-9]{3}",
+							},
+							"param3": {
+								DoesNotMatch: "[a-zA-Z]{3}",
+							},
+							"param4": {
+								Contains: "a",
+							},
+							"param5": {
+								DoesNotContain: "b",
+							},
+						},
+					},
+					Response: main.Response{
+						Status: 200,
+						Body:   `{"message": "This is a stub response", "param1"="{{.Query.param1}}", "param2"="{{.Query.param2}}", "param3"="{{.Query.param3}}", "param4"="{{.Query.param4}}", "param5"="{{.Query.param5}}"}` + "\n",
+					},
+				},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -279,8 +454,11 @@ func Test_loadConfig(t *testing.T) {
 				t.Errorf("loadConfig() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("loadConfig() = %v, want %v", got, tt.want)
+			// if !reflect.DeepEqual(got, tt.want) {
+			// 	t.Errorf("loadConfig() = %v, want %v", got, tt.want)
+			// }
+			if !cmp.Equal(got, tt.want) {
+				t.Errorf("diff: %v", cmp.Diff(got, tt.want))
 			}
 		})
 	}
